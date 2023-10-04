dbf727411bb4e6ffebe16a1c5ec8fe88a0cd0c3a
Salbi Faza <salbifaza13@gmail.com>
2023-10-04 14:42:20 +0700

    update

--- code/create_table.py
+++ code/create_table.py
@@ -1,12 +1,13 @@
 # ---------- Import packages ----------
 import os
+import csv
 import psycopg2
 
 # ---------- Connect to postgresql ----------
 conn = psycopg2.connect("host = localhost dbname = postgres user = postgres password= admin")
 cur = conn.cursor()
 
-# ---------- Create Table ----------
+# ---------- Create Table ---------- 
 cur.execute("""
             CREATE TABLE IF NOT EXISTS tbl_user(
               id serial PRIMARY KEY
@@ -15,8 +16,21 @@ cur.execute("""
             , phone text
             , postal_code text
             )
-
         """
 )
 
-path = os.getcwd()
+# ---------- Get File Directory ---------- 
+path      = os.getcwd()
+file      = '/source/users_w_postal_code.csv'
+file_path = path + file
+
+# ---------- Open File ---------- 
+with open(file_path,'r') as f:
+    csv_reader = csv.reader(f, delimiter = ',')
+    next(csv_reader)
+    for row in csv_reader:
+      cur.execute(
+         "INSERT INTO tbl_user VALUES (default, %s, %s, %s, %s) ON CONFLICT DO NOTHING",
+         row
+      )
+conn.commit()
